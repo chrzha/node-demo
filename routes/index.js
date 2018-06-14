1d4ffe318985d2c98aca972e377c9407eff97384
Chris.Zhang <chris.zhang@augmentum.com>
2018-06-14 12:10:52 +0800

    change the view engine from pug to ejs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var obj = {
+    title: 'Index',
+    users: [
+      {'name': 'a', 'age': 1, 'gender': 'M'},
+      {'name': 'b', 'age': 1, 'gender': 'M'},
+      {'name': 'c', 'age': 1, 'gender': 'M'},
+      {'name': 'd', 'age': 1, 'gender': 'M'},
+      {'name': 'e', 'age': 1, 'gender': 'M'},
+      {'name': 'f', 'age': 1, 'gender': 'M'}
+    ],
+    total: 100
+  };
+  res.render('index', obj);
 });
 
 module.exports = router;
